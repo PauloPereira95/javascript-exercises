6806be22cb627eb3168b57fd895403c263de4994
PauloPereira95 <paulohuawei95@gmail.com>
2023-02-11 16:31:45 +0000

    exe 01 e 02 resolvidos

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,nVezes) {
+    let stringAdd='';
+    if(nVezes < 0){
+        return stringAdd ='ERROR';
+    } else {
+        for (let i = 0; i < nVezes; i++) {
+            stringAdd += string;
+        }
+        return stringAdd;
+    }
 };
 
 // Do not edit below this line
