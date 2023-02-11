fd012e42fa73450b6d0cca3a1d17d9a9085d432b
PauloPereira95 <paulohuawei95@gmail.com>
2023-02-11 16:48:07 +0000

    exercico 03 resolvido

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr,arrayRev;
+    let stringFinal;
+    splitStr = str.split('');
+    arrayRev = splitStr.reverse();
+    stringFinal = arrayRev.join('');
+  return stringFinal;
 };
 
 // Do not edit below this line
