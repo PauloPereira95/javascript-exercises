a7598d85c19ddb091b5d10e4fbe30c4b3b120cba
PauloPereira95 <paulohuawei95@gmail.com>
2023-02-12 15:28:10 +0000

    exercicio 04 resolvido

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    let array = arguments[0];
+    let nDelete = [...args];
 
+       for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < nDelete.length; j++) {
+          if (nDelete.includes(arr[i])){
+            array.splice(i,1);
+          }
+         }
+        }
+      return array;
+  
 };
 
 // Do not edit below this line
