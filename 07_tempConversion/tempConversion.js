9ce4c1ebbf2e11cbbca2fdc989e5eaddd0ac8206
PauloPereira95 <paulohuawei95@gmail.com>
2023-02-14 21:39:55 +0000

    resolucaao com correcao

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fai) {
+  let temp = Math.round(((fai - 32) / 1.8) * 10) / 10;
+    //temp = temp.toFixed(1);
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let temp = Math.round(((cel * 1.8) + 32) * 10) / 10;
+  //temp =  temp.toFixed(1);
+
+  return temp;
 };
 
 // Do not edit below this line
